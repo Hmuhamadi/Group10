afa18712ec72d0c023b1695832696831c7e5be4e
Hmuhamadi <muhamadihavugimana@gmail.com>
2025-09-28 17:48:06 +0200

    fourth commit

---
+++ mh4.php
@@ -0,0 +1,22 @@
+<?php
+
+$a=10;
+$b=5;
+/*
+>echo
+>print
+>printf
+>sprintf
+>print_r
+>var_dump
+
+*/
+ 
+echo"sum of two numbers:".($a+$b)."<br>";
+print"different of two numbers:".($a-$b)."<br>";
+printf("products:".($a*$b)."<br>");
+$h=sprintf("products:".($a*$b)."<br>");
+echo"$h";
+print_r("divission:".$a/$b."<br>");
+var_dump("divission:".$a/$b."<br>");
+?> 
